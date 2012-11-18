6e2b9871ea185b5eb246ea0e550a245b82155243
stephen bailey <stephenn.bailey@gmail.com>
2012-11-18 15:06:00 +0000

    minor update to JobsOptions

--- Lancer/src/com/example/lancer/JobsOptions.java
+++ Lancer/src/com/example/lancer/JobsOptions.java
@@ -5,41 +5,41 @@ import android.app.Activity;
 import android.content.Intent;
 import android.view.Menu;
 
-public class JobOptions extends Activity {
+public class JobsOptions extends Activity {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_job_options);
+        setContentView(R.layout.activity_jobs_options);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_job_options, menu);
+        getMenuInflater().inflate(R.menu.activity_jobs_options, menu);
         return true;
     }
       	
     public void createInvoice()
     {
-    	Intent createIncoice = new Intent(JobOptions.this, CreateInvoice.class);
+    	Intent createIncoice = new Intent(JobsOptions.this, CreateInvoice.class);
     	startActivity(createIncoice);
     }
     	
     public void viewTasks()
     {
-    	Intent viewTasks = new Intent(JobOptions.this, TasksList.class);
+    	Intent viewTasks = new Intent(JobsOptions.this, TasksList.class);
     	startActivity(viewTasks); 
     }
     
     public void viewExpences()
     {
-    	Intent viewExpences = new Intent(JobOptions.this, ExpensesList .class);
+    	Intent viewExpences = new Intent(JobsOptions.this, ExpensesList .class);
     	startActivity(viewExpences);
     }
     
     public void deleteJob()
     {
-    	Intent deleteJob = new Intent(JobOptions.this, DeleteJob.class);
+    	Intent deleteJob = new Intent(JobsOptions.this, DeleteJob.class);
     	startActivity(deleteJob);
     }
        
