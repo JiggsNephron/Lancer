346b3e175af4a343526bed5f297acba85fd189c6
Richard Cody <thelostnephron@gmail.com>
2012-11-19 15:39:37 +0000

    Small changes

--- Lancer/src/com/zenfly/lancer/AddNewJob.java
+++ Lancer/src/com/zenfly/lancer/AddNewJob.java
@@ -1,13 +1,13 @@
 package com.zenfly.lancer;
 
 import com.example.lancer.R;
-import com.example.lancer.R.layout;
-import com.example.lancer.R.menu;
 
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 import android.support.v4.app.NavUtils;
 
 public class AddNewJob extends Activity {
@@ -16,8 +16,16 @@ public class AddNewJob extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_new_job);
-        getActionBar().setDisplayHomeAsUpEnabled(true);
+        
+        EditText jobName = (EditText) findViewById(R.id.job_name);
+        EditText jobLocation = (EditText) findViewById(R.id.job_name);
+        EditText jobDeadline = (EditText) findViewById(R.id.job_name);
     }
+    
+    // on pressing Back, go back to main
+    public void saveJob(View v) {
+    	finish();
+    }    
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
