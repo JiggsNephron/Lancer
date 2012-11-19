9f6e2b9cb7bf386ac42088f4f70f331169b94db5
Richard Cody <thelostnephron@gmail.com>
2012-11-19 16:17:46 +0000

    AddNewJob.java done as far as currently do-able. Paused until McD adds support to the database for Job deadlines.

--- Lancer/src/com/zenfly/lancer/AddNewJob.java
+++ Lancer/src/com/zenfly/lancer/AddNewJob.java
@@ -4,6 +4,7 @@ import com.example.lancer.R;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -15,18 +16,35 @@ public class AddNewJob extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_add_new_job);
-        
-        EditText jobName = (EditText) findViewById(R.id.job_name);
-        EditText jobLocation = (EditText) findViewById(R.id.job_name);
-        EditText jobDeadline = (EditText) findViewById(R.id.job_name);
+        setContentView(R.layout.activity_add_new_job);    	
+    	
     }
     
     // on pressing Back, go back to main
     public void saveJob(View v) {
-    	finish();
+    	
+    	DatabaseHandler db = new DatabaseHandler(this);
+    	Intent backToJobsList = new Intent(this, JobsList.class);
+    	
+        // Getting each EditText
+        EditText jobName = (EditText) findViewById(R.id.job_name);
+        EditText jobLocation = (EditText) findViewById(R.id.job_location);
+        EditText jobDeadline = (EditText) findViewById(R.id.job_deadline); 	// not yet used
+        
+        // get the data from each element and store it
+    	String stjobName = jobName.getText().toString();
+    	String stjobLocation = jobLocation.getText().toString();
+    	String stjobDeadline = jobDeadline.getText().toString(); 			// not yet used
+    	
+    	// add new job based on user entered data    		
+    	db.addJob(new Job(stjobName, stjobLocation)); 						// TODO: need to be able to add deadline
+    	
+    	startActivity(backToJobsList);
+    	
     }    
-
+    
+    /** OPTIONS MENU CODE DISABLED FOR NOW
+    
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.activity_add_new_job, menu);
@@ -43,5 +61,6 @@ public class AddNewJob extends Activity {
         }
         return super.onOptionsItemSelected(item);
     }
-
+    
+	**/
 }
