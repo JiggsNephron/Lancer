d769327f99e156f9501925824f5afc63fc815e48
Richard Cody <thelostnephron@gmail.com>
2012-11-20 00:37:05 +0000

    Manifest change

--- Lancer/src/com/zenfly/lancer/AddNewTask.java
+++ Lancer/src/com/zenfly/lancer/AddNewTask.java
@@ -11,11 +11,13 @@ import java.util.List;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Spinner;
 
@@ -60,6 +62,23 @@ public class AddNewTask extends Activity {
 			AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(context);
 			
 			alertDialogBuilder.setView(promptsView);
+			
+			final DatePicker datePicker = (DatePicker) findViewById( R.id.picker_task_deadline );
+			
+			alertDialogBuilder.setCancelable(false).setPositiveButton("OK", new DialogInterface.OnClickListener() {
+			    public void onClick(DialogInterface dialog,int id) {
+			    	// get user input and set it to result
+			    	// edit text
+			    	add_deadline.setText(Integer.toString(datePicker.getDayOfMonth()) + "/" + Integer.toString(datePicker.getMonth()) + "/" + Integer.toString(datePicker.getYear()));
+			    }
+			  })
+			.setNegativeButton("Cancel",
+			  new DialogInterface.OnClickListener() {
+			    public void onClick(DialogInterface dialog,int id) {
+				dialog.cancel();
+			    }
+			  });
+			
 			
 			// create alert dialog
 			AlertDialog alertDialog = alertDialogBuilder.create();
