b0b21fd589bb9bafc9b649ea8eefbd298c965cf5
Richard Cody <thelostnephron@gmail.com>
2012-11-21 22:17:12 +0000

    Created a temporary LocationsAdapter

---
+++ Lancer/src/com/zenfly/lancer/LocationsAdapter.java
@@ -0,0 +1,87 @@
+package com.zenfly.lancer;
+
+
+import java.util.List;
+
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+
+public class LocationsAdapter extends ArrayAdapter<Job>
+{
+	//public static final String PREFS_COUNT = "MyPrefsFile";
+	  private final Activity activity;
+	  private final List<Job> jobsObject;
+	  public DatabaseHandler db;
+	
+	public LocationsAdapter(Activity activity, List<Job> objects) 
+	{
+        super(activity, R.layout.activity_jobs_list , objects);
+        this.activity = activity;
+        this.jobsObject = objects;
+        db = new DatabaseHandler(this.getContext());
+	} 
+
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent)
+	{
+	    View rowView = convertView;
+	    jobsView jobsItemView = null;
+	
+	    if(rowView == null)
+	    {
+	    	//creates new instance of row layout view
+	        LayoutInflater inflater = activity.getLayoutInflater();
+	        rowView = inflater.inflate(R.layout.job_item, null);
+	        jobsItemView = new jobsView(); //for holding the data
+	        jobsItemView.name = (TextView) rowView.findViewById(R.id.JobNameDisplay); 
+	        jobsItemView.location = (TextView) rowView.findViewById(R.id.JobLocationDisplay);
+	        jobsItemView.date = (TextView) rowView.findViewById(R.id.JobDateDisplay);
+	       
+	        rowView.setTag(jobsItemView); //for later access
+	    }
+	    else jobsItemView = (jobsView) rowView.getTag();
+	    {
+	    	Job currentJob = (Job) jobsObject.get(position); //casts as course
+	    	
+	    	
+	    	if(db.getJobTaskCount(currentJob.getId()) != 0)
+	    	{
+		    	Task tempTask = db.getNearestDeadlineTask();
+		    	
+		    	int tempLocation = tempTask.getLocation();// finds the location in the data base we are looking for
+		    	Location a  = db.getLocation(tempLocation); // extracts the location from the database
+		    	String b = a.getLocation(); // puts the location into a string
+		    	
+		    	String tempDate = tempTask.getDeadline();// just returns the raw date string
+		    	
+		    	
+		    	jobsItemView.name.setText("Test");//currentJob.getClient()); //sets the data
+		    	jobsItemView.location.setText(b);				   //sets the data
+		    	jobsItemView.date.setText(tempDate);			   //sets the data
+		    	return rowView;
+	    	}
+	    	else
+	    	{
+	    		jobsItemView.location.setText("");
+	    		jobsItemView.date.setText("");
+	    	}
+	    	jobsItemView.name.setText(currentJob.getClient()); //sets the data
+	    }
+	    return rowView;
+	}
+
+    protected static class jobsView
+    {
+        protected TextView name;
+        protected TextView location;
+        protected TextView date;
+
+    }
+}
+
+
