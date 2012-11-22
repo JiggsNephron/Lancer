c89ec6a56a9696ae0c630cb1f2189c3837351216
Richard Cody <thelostnephron@gmail.com>
2012-11-22 13:54:21 +0000

    Cleaned AddNewTask up

--- Lancer/src/com/zenfly/lancer/AddNewTask.java
+++ Lancer/src/com/zenfly/lancer/AddNewTask.java
@@ -40,6 +40,11 @@ public class AddNewTask extends FragmentActivity {
 	EditText add_deadline;
 	DatabaseHandler db;
 	
+	String sttask_name;
+	String sttask_date;
+	int task_location_id;
+	int job_id;
+	
 	Calendar calendar;
 	int year, month, day;
 	
@@ -48,23 +53,36 @@ public class AddNewTask extends FragmentActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_new_task);
-        task_name  = (EditText) findViewById(R.id.task_name);
-        task_location_box  = (EditText)findViewById(R.id.button_location_names);//button_add_new_location);
-        add_deadline = (EditText)findViewById(R.id.button_add_deadline);
+        
+        calendar = Calendar.getInstance(); 
         db = new DatabaseHandler(this.getApplicationContext());
-        if(getIntent().getStringExtra("task_name") != null) task_name.setText(getIntent().getStringExtra("task_name"));
-        if(getIntent().getStringExtra("task_date") != null) add_deadline.setText(getIntent().getStringExtra("task_date"));
-        if(getIntent().getIntExtra("location", 0) != 0) 
-        {
-        	Location location = db.getLocation(getIntent().getIntExtra("location", 0));
-        	task_location_box.setText(location.getLocation());
+        
+        task_name  = (EditText) findViewById(R.id.task_name);
+        task_location_box  = (EditText)findViewById(R.id.button_location_names);
+        add_deadline = (EditText)findViewById(R.id.button_add_deadline);
+        
+        job_id = getIntent().getIntExtra("job_id", 0);
+        
+        // if there has been a received intent and it has the task name, put it into the EditText
+        if(getIntent().getStringExtra("task_name") != null) {
+        	sttask_name = task_name.getText().toString();
+        	task_name.setText(sttask_name);
         }
-        calendar = Calendar.getInstance();
-        
-        
-        if(getIntent().getStringExtra("task_date") != null) add_deadline.setText(getIntent().getStringExtra("task_date"));
-        // FIXME RC: FOR SK > change here to show something else on the deadline EditText view when first shown
-        else add_deadline.setText("Choose a Deadline");
+        // if there has been a received intent and it has the task location ID, put it into the EditText        
+        if(getIntent().getIntExtra("location", 0) != 0) {
+        	task_location_id = getIntent().getIntExtra("location", 0);
+        	Location location = db.getLocation(task_location_id);
+        	task_location_box.setText(location.getLocation());
+        }              
+        // if there has been a received intent and it has the task date, put it into the EditText
+        // otherwise just add an instruction
+        if(getIntent().getStringExtra("task_date") != null) {
+        	sttask_date = getIntent().getStringExtra("task_date");
+        	add_deadline.setText(sttask_date);        
+        }
+        else {
+        	add_deadline.setText("Choose a Deadline"); // FIXME RC: FOR SK > change here to show something else on the deadline EditText view when first shown
+        }
     }
     
     // OnClick of the Deadline EditText
@@ -92,99 +110,36 @@ public class AddNewTask extends FragmentActivity {
     		populateSetDate(yy, mm+1, dd);
     	}
     }
-    
+
+    // Saves the all the chosen entries as a new task
     public void saveTask (View v) {
-    	Task task = new Task(task_name.getText().toString(), getIntent().getIntExtra("job", 0), add_deadline.getText().toString(), getIntent().getIntExtra("location", 0), 0, 0);
+    	Task task = new Task(sttask_name, job_id, add_deadline.getText().toString(), task_location_id, 0, 0);
     	db.addTask(task); 
     	Intent intent = new Intent(AddNewTask.this, JobsOptions.class);
     	intent.putExtra("job", getIntent().getIntExtra("job", 0));
     	startActivity(intent);
     }
     
-    // TODO RC: ???move chooseLocation to its own class???
+    // Lets the user choose from their list of saved locations (or add a new one)
     public void chooseLocation(View v) {    	
     	
     	Intent show_locations = new Intent(this, LocationsList.class);
     	
-    	String sttask_name = task_name.getText().toString();
-    	String sttask_date = add_deadline.getText().toString();
+    	// Preserve the already entered options
+    	sttask_name = task_name.getText().toString();
+    	sttask_date = add_deadline.getText().toString();
+    	
+    	// Forward the saved entries to the locations list activity
+    	// which then sends it back to re-populate those fields in this activity
     	show_locations.putExtra("task_name", sttask_name);
     	show_locations.putExtra("task_date", sttask_date);
-    	show_locations.putExtra("job", getIntent().getIntExtra("job", 0));
+    	show_locations.putExtra("job_id", job_id);
     	
+    	// show the locations list
     	startActivity(show_locations);
-    	
-    	// TODO RC: send the already filled data via addExtra and then receive the location thats been chosen
-    	
-//    	LayoutInflater li = LayoutInflater.from(context);
-//		View promptsView = li.inflate(R.layout.activity_locations_list, null);		
-//		
-//		AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(context);
-//		
-//		alertDialogBuilder.setView(promptsView);
-//		
-//		// create alert dialog
-//		AlertDialog alertDialog = alertDialogBuilder.create();
-//		
-//		List<Location> locations = new ArrayList<Location>();
-//		
-//		DatabaseHandler db = new DatabaseHandler(AddNewTask.this);
-//		locations = db.getAllLocations();
-//   
-//		//setListAdapter(new LocationsAdapter(AddNewTask.this, locations));
-//
-//		// show it
-//		alertDialog.show();
-		
+	
     }
-    
-    
-//    public void addLocation(View v) {   	
-//    	
-//    	// create new dialog object
-//    	final Dialog dialog = new Dialog(AddNewTask.this);
-//    	
-//    	// set the correct layout for this dialog
-//    	dialog.setContentView(R.layout.activity_add_new_location);
-//    	dialog.setTitle("Add a new location");    	
-//
-//    	// find the save button
-//		Button button = (Button) dialog.findViewById(R.id.saveLocation);
-//		
-//		
-//		
-//		// what to do when the button is clicked
-//		button.setOnClickListener(new OnClickListener() {
-//			// get each EditText field, put the contents to string, and then save these to the locations database
-//			public void onClick(View v) {
-//				
-//				DatabaseHandler db = new DatabaseHandler(AddNewTask.this); 
-//				
-//		    	EditText location_nickname  = (EditText)dialog.findViewById(R.id.location_name);
-//		    	EditText location_address1  = (EditText)dialog.findViewById(R.id.address_line1);
-//		    	EditText location_address2  = (EditText)dialog.findViewById(R.id.address_line2);
-//		    	EditText location_address3  = (EditText)dialog.findViewById(R.id.address_line3);
-//		    	
-//		    	String stlocation_nickname = location_nickname.getText().toString();
-//		    	String stlocation_address1 = location_address1.getText().toString();
-//		    	String stlocation_address2 = location_address2.getText().toString();
-//		    	String stlocation_address3 = location_address3.getText().toString();
-//		    	
-//		    	if(!stlocation_nickname.equals(""))
-//		    	{
-//			    	// add new location based on user entered data
-//		    		db.addLocation(new Location(stlocation_nickname, stlocation_address1, stlocation_address2, stlocation_address3));
-//		    		Toast.makeText(getApplicationContext(), "Saved Location: " + stlocation_nickname, Toast.LENGTH_LONG).show();
-//		    		dialog.dismiss();
-//		    	} else if (stlocation_nickname.equals("")) {
-//		    		Toast.makeText(getApplicationContext(), "Location Nickname cannot be empty", Toast.LENGTH_LONG).show();		    		
-//		    	}		        
-//		     }
-//		 });	
-//		
-//		// show the dialog
-//		dialog.show();		
-//    }   
+ 
         
     /** OPTIONS MENU CODE DISABLED FOR NOW
     
