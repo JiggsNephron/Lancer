38924e418356b5fedab5eaff3f71355278e797e2
Richard Cody <thelostnephron@gmail.com>
2012-11-28 17:33:56 +0000

    Adding a new not should work. There's no NotesList yet, so I can test it right now.

--- Lancer/src/com/zenfly/lancer/AddNewNote.java
+++ Lancer/src/com/zenfly/lancer/AddNewNote.java
@@ -1,15 +1,92 @@
 package com.zenfly.lancer;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.view.Window;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 public class AddNewNote extends Activity {
+	
+	final Context context = this;
+	
+	DatabaseHandler db;
+	
+	EditText et_note_subject;
+	EditText et_note_body;
+	Spinner sp_assign_to_task;
+	
+	String stnote_subject;
+	String stnote_body;
+	
+	int job_id;
+	int task_id;
+	
+	Task chosen_task;
+	
+	List<Task> all_tasks = new ArrayList<Task>();
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_add_new_note);
+        
+        db = new DatabaseHandler(context);
+        
+        job_id = getIntent().getIntExtra("job_id", 0);
+        
+        et_note_subject  = (EditText) findViewById(R.id.note_subject);
+        et_note_body  = (EditText) findViewById(R.id.note_body);
+        sp_assign_to_task  = (Spinner)findViewById(R.id.note_to_task);
+        
+        // get a list of all tasks to be used for populating the spinner
+        all_tasks = db.getAllTasksForJob(job_id);
+        // create an ArrayAdapter to fill with task names
+        ArrayAdapter <CharSequence> adapter = new ArrayAdapter <CharSequence> (this, android.R.layout.simple_spinner_item );
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // add one default task choice (no task chosen)
+        adapter.add("Not assigned to a task");
+        // go through the list of tasks, and for each one, get its name and add it to the adapter array
+        for (Task task: all_tasks) {
+        	adapter.add(task.getName());
+        }      
+        // set the spinner to use the array contents       
+        sp_assign_to_task.setAdapter(adapter);
+        
+        
+        
+    }
+    
+    public void saveNote (View v) {
+    	
+    	//Intent intent = new Intent(context, NotesList.class);
+    	
+    	stnote_body = et_note_body.getText().toString();
+    	stnote_subject = et_note_subject.getText().toString();
+    	
+    	// get the ID of the task chosen by the user in the spinner by spinner's position
+    	// if no task was chosen, 0 is used
+    	if ((sp_assign_to_task.getSelectedItemPosition()-1) >= 1) {
+    		chosen_task = all_tasks.get(sp_assign_to_task.getSelectedItemPosition()-1);
+    		task_id = chosen_task.getId();
+    	} else task_id = 0;
+    	
+    	Note new_note = new Note(job_id, task_id, stnote_subject, stnote_body);   	   	
+    	
+		db.addNote(new_note);
+		
+		//intent.putExtra("job_id", job_id);
+		//startActivity(intent);
+    	
     }
 
     @Override
