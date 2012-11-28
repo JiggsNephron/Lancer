de3d3a2c029836dd047a0e00dee5a5d8750baac2
Richard Cody <thelostnephron@gmail.com>
2012-11-28 15:49:58 +0000

    import readded

--- Lancer/src/com/zenfly/lancer/AddNewExpense.java
+++ Lancer/src/com/zenfly/lancer/AddNewExpense.java
@@ -9,6 +9,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
