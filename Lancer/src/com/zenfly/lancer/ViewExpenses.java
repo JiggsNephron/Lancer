62817195f1810ba0e89c28554313f307c6ebb85d
stephen bailey <stephenn.bailey@gmail.com>
2012-11-30 12:27:48 +0000

    mostly working view Expenses ok its shit but it works . . . mostly

--- Lancer/src/com/zenfly/lancer/ViewExpenses.java
+++ Lancer/src/com/zenfly/lancer/ViewExpenses.java
@@ -1,12 +1,11 @@
 package com.zenfly.lancer;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.Window;
+import android.widget.TextView;
 
 public class ViewExpenses extends Activity {
 	
@@ -27,18 +26,50 @@ public class ViewExpenses extends Activity {
 ///*	
  		db = new DatabaseHandler(this);
 		expenseId = getIntent().getExtras().getInt("expense"); 				// gets the task ID from the intent
-		expense  =  db.getExpense(expenseId);									// gets the Task object from the database
-		item     =  db.getItem(expense.getItem());
-		//task     =  db.getTask(expense.getTask());  //optional 
+		expense   =  db.getExpense(expenseId);									// gets the Task object from the database
+		item      =  db.getItem(expense.getItem());
+		int TaskId    =  expense.getTask();
+		String linkedTask = "";
+		Log.v("View Expenses", "expense.getTask() =" + expense.getTask() + "~END");
+		if(TaskId != 0)
+		{
+			task     =  db.getTask(TaskId);  //optional 
+			linkedTask = task.getName();
+		}
+
 		
 		String ItemName = item.getName();								//extracts the name from Object
 		float ItemPrice = item.getPrice();						//extracts the due Date from Object
-	//	String linkedTask = task.getName();
 		int Quantity = expense.getQuantity();
 		
+		float TotalCost = Quantity * ItemPrice;
 		
+		String QuantityString = Integer.toString(Quantity);
+		String PriceString = String.valueOf(ItemPrice);
+		String TotalCostString = String.valueOf(TotalCost);
+		
+		
+		/*
+		TextView JobNameTitle = (TextView) findViewById(R.id.job_name);				//prepares to access textView
+		TextView TaskNameTitle = (TextView) findViewById(R.id.job_option);			//prepares to access textView
+		TextView displayName = (TextView) findViewById(R.id.thisTaskName);			//prepares to access textView
 		//*/
+		TextView ExpenseItemDisplay = (TextView) findViewById(R.id.ExpenseItemDisplay);		//prepares to access textView
+		TextView ExpenseCostDisplay = (TextView) findViewById(R.id.ExpenseCostDisplay);	//prepares to access textView
+		TextView IndividualCost = (TextView) findViewById(R.id.IndividualCost);		//prepares to access textView
+		TextView ItemQuantity = (TextView) findViewById(R.id.ItemQuantity);	//prepares to access textView
+		TextView TaskName = (TextView) findViewById(R.id.TaskName);		//prepares to access textView
 		
+		
+	//	JobNameTitle.setText(JobName);									// sets the text view this data will always be set
+	//	TaskNameTitle.setText("View Task");								// sets the text view this data will always be set
+	//	displayName.setText(taskName);									// sets the text view this data will always be set
+		
+		ExpenseItemDisplay.setText(ItemName);									// sets the text view this data will always be set
+		ExpenseCostDisplay.setText(TotalCostString);								// sets the text view this data will always be set
+		IndividualCost.setText(PriceString);									// sets the text view this data will always be set
+		ItemQuantity.setText(QuantityString);									// sets the text view this data will always be set
+		TaskName.setText(linkedTask);								// sets the text view this data will always be set
 	
 	}
 
