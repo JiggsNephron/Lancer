38e16757ea3c8a25d2c7d4bb8484138fb428b87f
Simon Keating <simon@laidbear.com>
2012-12-01 11:43:18 +0000

    Added Splash Screen. Clearly labelled code for it incase any issues.

--- Lancer/src/com/zenfly/lancer/JobsList.java
+++ Lancer/src/com/zenfly/lancer/JobsList.java
@@ -3,11 +3,13 @@ package com.zenfly.lancer;
 import java.util.ArrayList;
 import java.util.List;
 
+import android.app.Dialog;
 import android.app.ListActivity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.os.Handler;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -21,29 +23,46 @@ public class JobsList extends ListActivity {
 	List<Job> jobs = new ArrayList<Job>();
 	SharedPreferences prefs;
 	int backpress = 0;
+	protected Dialog mSplashDialog; // Needed for SplashScreen
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-       // this.requestWindowFeature(Window.FEATURE_NO_TITLE); // RC: this removed the black bar at the top of activities. 
-        setContentView(R.layout.activity_jobs_list);
-        String order;
-        prefs = getSharedPreferences("com.zenfly.lancer",0);
-        db = new DatabaseHandler(this.getApplicationContext());;
-        if(db.getJobCount() == 0)
-        {
-        	Intent intent = new Intent(JobsList.this, AddNewJob.class);
-        	Toast.makeText(getApplicationContext(), "You have no current jobs. Please add one", Toast.LENGTH_LONG).show();
-        	startActivity(intent);
-        }
-        order = prefs.getString("order", "none");
-        if(order.equals("none"))jobs = db.getAllJobs();
-        else if(order.equals("deadline"))jobs = db.getAllJobsByDeadline();
-        else if(order.equals("alphaAsc"))jobs = db.getAllJobsByAlphaAsc();
-        else if(order.equals("alphaDesc"))jobs = db.getAllJobsByAlphaDesc();
+       
+        // this.requestWindowFeature(Window.FEATURE_NO_TITLE); // RC: this removed the black bar at the top of activities. 
         
-        setListAdapter(new JobsAdapter(this, jobs)); //starts the list View
-    }
+        //Splash Code Start
+        MyStateSaver data = (MyStateSaver) getLastNonConfigurationInstance(); // Method checks if app is still loading
+	    if (data != null) {
+	    	if (data.showSplashScreen) {
+	            showSplashScreen();
+	        }
+	    	setContentView(R.layout.activity_jobs_list);
+	    	} else {
+	        showSplashScreen();
+	        setContentView(R.layout.activity_jobs_list);
+	        //Splash Code end
+	        
+	        String order;
+	        prefs = getSharedPreferences("com.zenfly.lancer",0);
+	        db = new DatabaseHandler(this.getApplicationContext());;
+	        if(db.getJobCount() == 0)
+	        {
+	        	Intent intent = new Intent(JobsList.this, AddNewJob.class);
+	        	Toast.makeText(getApplicationContext(), "You have no current jobs. Please add one", Toast.LENGTH_LONG).show();
+	        	startActivity(intent);
+	        }
+	        order = prefs.getString("order", "none");
+	        if(order.equals("none"))jobs = db.getAllJobs();
+	        else if(order.equals("deadline"))jobs = db.getAllJobsByDeadline();
+	        else if(order.equals("alphaAsc"))jobs = db.getAllJobsByAlphaAsc();
+	        else if(order.equals("alphaDesc"))jobs = db.getAllJobsByAlphaDesc();
+	        
+	        setListAdapter(new JobsAdapter(this, jobs)); //starts the list View
+	        
+	    	}
+		}
+	 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -97,8 +116,8 @@ public class JobsList extends ListActivity {
 	    this.finish();
 	  }
 	
-	  //below is the code that creates the menu using the right xml file
-	// /*
+	  // Below is the code that creates the menu using the right xml file
+	
 		@Override
 	  	public boolean onOptionsItemSelected(MenuItem item) {
 	      // Handle item selection
@@ -110,8 +129,56 @@ public class JobsList extends ListActivity {
 	          default:
 	              return super.onOptionsItemSelected(item);
 	      }
-	  }//*/
+	  }
 	
+		
+		//Splash Code Start
+		@Override
+		public Object onRetainNonConfigurationInstance() {
+		    MyStateSaver data = new MyStateSaver();
+		
+		    if (mSplashDialog != null) {
+		        data.showSplashScreen = true;
+		        removeSplashScreen();
+		    }
+		    return data;
+		}
+		 
+	
+	 // Removes the Dialog that displays the splash screen
+		 
+		protected void removeSplashScreen() {
+		    if (mSplashDialog != null) {
+		        mSplashDialog.dismiss();
+		        mSplashDialog = null;
+		    }
+		}
+		 
+		
+		//Shows the splash screen over the full Activity
+		 
+		protected void showSplashScreen() {
+		    mSplashDialog = new Dialog(this, R.style.SplashScreen); // From res/values/styles.xml
+		    mSplashDialog.setContentView(R.layout.splashscreen);
+		    mSplashDialog.setCancelable(false);
+		    mSplashDialog.show();
+		     
+		    // Set Runnable to remove splash screen just in case
+		    final Handler handler = new Handler();
+		    handler.postDelayed(new Runnable() {
+		      
+		      public void run() {
+		        removeSplashScreen();
+		      }
+		    }, 3000);
+		}
+		 
+		
+		// Simple class for storing important data across config changes
+		 
+		private class MyStateSaver {
+		    public boolean showSplashScreen = false;
+		}	
+		//SplashCode End
+		
 }
-
-// a test comment
