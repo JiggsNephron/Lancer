097527a3b84cd139c07dc68cc85e59eeb5e5582a
Richard Cody <thelostnephron@gmail.com>
2012-12-01 17:47:14 +0000

    EditNote temporarily done. Needs testing. Spinner should set itself correctly if note is assigned to a task

--- Lancer/src/com/zenfly/lancer/EditNote.java
+++ Lancer/src/com/zenfly/lancer/EditNote.java
@@ -11,6 +11,7 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 public class EditNote extends Activity {
 
@@ -28,6 +29,9 @@ public class EditNote extends Activity {
 	int job_id;
 	int task_id;
 	int note_id;
+	
+	int counter = 0;
+	int set_spinner_to = 0;
 	
 	Note current_note;
 	Task chosen_task;
@@ -63,17 +67,46 @@ public class EditNote extends Activity {
         adapter.add("Not assigned to a task");
         // go through the list of tasks, and for each one, get its name and add it to the adapter array
         for (Task task: all_tasks) {
+        	counter++;
         	adapter.add(task.getName());
+        	if (task.getId() == task_id) {
+        		set_spinner_to = counter;
+        	}
         }      
         // set the spinner to use the array contents       
         sp_assign_to_task.setAdapter(adapter);
+        // set the spinner to the postion of the already set task
+        sp_assign_to_task.setSelection(set_spinner_to);
     }
     
     public void saveNoteEdits (View v) {
+    	
     	Intent back_to_note = new Intent(context, ViewNotes.class);
-    	back_to_note.putExtra("job_id", job_id);
-    	startActivity(back_to_note);
+    	
+    	// get the user entered note body and subject and put to string
+    	stnote_body = et_note_body.getText().toString();
+    	stnote_subject = et_note_subject.getText().toString();
+    	
+    	// get the ID of the task chosen by the user in the spinner by spinner's position
+    	// if no task was chosen, 0 is used
+    	if ((sp_assign_to_task.getSelectedItemPosition()-1) >= 1) {
+    		chosen_task = all_tasks.get(sp_assign_to_task.getSelectedItemPosition()-1);
+    		task_id = chosen_task.getId();
+    	} else task_id = 0;
+    	
+    	// if the body and subject are populated, the note is saved with the changes
+    	// if not, a toast is shown to inform the user
+    	if (!stnote_body.equals("") || !stnote_subject.equals("")) {
+    		
+    		Note new_note = new Note(job_id, task_id, stnote_subject, stnote_body);        	
+    		db.addNote(new_note);
+    		Toast.makeText(getApplicationContext(), "Note " + stnote_subject + " added.", Toast.LENGTH_SHORT).show();
+    		back_to_note.putExtra("job_id", job_id);
+        	startActivity(back_to_note);
+    		
+    	} else Toast.makeText(getApplicationContext(), "Please enter a note subject and body", Toast.LENGTH_SHORT).show();
     }
+    
     public void deleteNote (View v) {
     }
 
