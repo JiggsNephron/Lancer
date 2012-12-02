51d87d888d15657f4715604e2ba059bdf17ba068
Simon McDonnell <simonmcdonnell1@gmail.com>
2012-12-02 22:02:42 +0000

    Deleting a location now changes any task set to that location to location=0

--- Lancer/src/com/zenfly/lancer/DatabaseHandler.java
+++ Lancer/src/com/zenfly/lancer/DatabaseHandler.java
@@ -727,6 +727,9 @@ public class DatabaseHandler extends SQLiteOpenHelper
     {
         SQLiteDatabase db = this.getWritableDatabase();
         db.delete(TABLE_LOCATIONS, KEY_ID + " = ?", new String[] { Integer.toString(id) });
+        ContentValues values = new ContentValues();
+        values.put(KEY_LOCATION, id);
+        db.update(TABLE_TASKS, values, KEY_LOCATION + "=?", new String[] { Integer.toString(id)});
         db.close();
     }
     
