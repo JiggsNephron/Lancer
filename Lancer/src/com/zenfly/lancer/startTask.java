0481a917f7b87dc220894b70891911a22b9347ba
Simon McDonnell <simonmcdonnell1@gmail.com>
2012-12-02 16:30:30 +0000

    Added getter and setter methods for the DB for taskStarted

--- Lancer/src/com/zenfly/lancer/startTask.java
+++ Lancer/src/com/zenfly/lancer/startTask.java
@@ -1,8 +1,11 @@
 package com.zenfly.lancer;
 
+import java.util.Calendar;
+
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 
 public class startTask extends BroadcastReceiver 
 {
@@ -10,10 +13,18 @@ public class startTask extends BroadcastReceiver
 	@Override
 	public void onReceive(Context context, Intent intent)
 	{
+		SharedPreferences prefs = context.getSharedPreferences("com.zenfly.lancer",0);
 		DatabaseHandler db = new DatabaseHandler(context);
 		int jobId = intent.getIntExtra("job_id", 0);
 		int taskId = intent.getIntExtra("task_id", 0);
-		
+		Calendar cal = Calendar.getInstance();
+		if(db.getTaskStarted(taskId) == 0)
+		{
+			
+		}
+		else
+		{
+			
+		}
 	}
 }
-
