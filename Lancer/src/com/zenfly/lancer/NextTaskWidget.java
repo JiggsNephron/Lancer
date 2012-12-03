030f7b015fcb399c17a86ad90ed0c9feaaf17b35
Simon McDonnell <simonmcdonnell1@gmail.com>
2012-12-03 23:53:26 +0000

    Widget works but when clicked will take to JobsList rather than ViewTask because it doesn't pass the ids for some reason

--- Lancer/src/com/zenfly/lancer/NextTaskWidget.java
+++ Lancer/src/com/zenfly/lancer/NextTaskWidget.java
@@ -1,9 +1,16 @@
 package com.zenfly.lancer;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.ComponentName;
 import android.content.Context;
+import android.content.Intent;
 import android.widget.RemoteViews;
 
 public class NextTaskWidget extends AppWidgetProvider {
@@ -11,16 +18,51 @@ public class NextTaskWidget extends AppWidgetProvider {
 	DatabaseHandler db;
 
 	@Override
-	public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
-		
+	public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds)
+	{	
 		ComponentName thisWidget = new ComponentName(context, NextTaskWidget.class);
-		
+		db = new DatabaseHandler(context);
+		String formatDate;
 		int[] allWidgetIds = appWidgetManager.getAppWidgetIds(thisWidget);
-		
-		RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.activity_next_task_widget);
-		
-		remoteViews.setTextViewText(R.id.job_name, "A test");
-		
+		Task task = db.getNearestDeadlineTask();
+		for (int widgetId : allWidgetIds)
+	    {
+		  	RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.activity_next_task_widget);
+		    if(task == null) remoteViews.setTextViewText(R.id.job_name, "No upcoming tasks");
+		     // else //in this case we know at least one module is returned
+		     // {
+		    else
+		    {
+		    	Intent intent = new Intent(context, JobsList.class);
+		    	intent.putExtra("task", task.getId());
+		    	intent.putExtra("job_id", task.getJob());
+		    	PendingIntent pending = PendingIntent.getActivity(context, 0, intent, 0);
+		    	remoteViews.setOnClickPendingIntent(R.id.layout, pending);
+		    	Job job = db.getJob(task.getJob());
+		    	remoteViews.setTextViewText(R.id.job_name, job.getClient());
+		    	remoteViews.setTextViewText(R.id.next_task_name, task.getName());
+		    	if(!task.getDeadline().equals(""))
+		    	{
+		    		SimpleDateFormat date_formater = new SimpleDateFormat("yyyy/MM/dd");			
+		    		try 
+		    		{
+		    			// creates a date object based on the SimpleDateFormat object
+		    			Date date_locale = date_formater.parse(task.getDeadline());
+				    	// formats the date to a locale friendly string and saves it
+						formatDate = DateFormat.getDateInstance().format(date_locale);	    		
+			    	  } catch (ParseException e)
+			    	  {
+			    		  formatDate = "None Set";
+					  }	    				
+					remoteViews.setTextViewText(R.id.next_task_deadline, formatDate); //displays the locale formatted deadline
+				}
+		    	else 
+				{
+					remoteViews.setTextViewText(R.id.next_task_deadline, "None Set");
+		        }
+		      appWidgetManager.updateAppWidget(widgetId, remoteViews);
+		    }
+	    }
 	}
 }
 
