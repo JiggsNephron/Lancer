01edac57eca2e4237956a8ec301659f18235fa96
Richard Cody <thelostnephron@gmail.com>
2012-12-04 01:11:18 +0000

    Cleaned up

--- Lancer/src/com/zenfly/lancer/ExpensesList.java
+++ Lancer/src/com/zenfly/lancer/ExpensesList.java
@@ -6,10 +6,8 @@ import java.util.List;
 
 import android.app.ListActivity;
 import android.content.Intent;
-import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Menu;
 import android.view.View;
 import android.view.Window;
 import android.widget.ListView;
